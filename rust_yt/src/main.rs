f3c9d91d90be5e7cbf252e8f27cb79eb18d61dab
wls503pl <peile.wu.1990@gmail.com>
2026-03-23 19:21:18 +0800

    Chapter 3: Functions and Closures.

--- rust_yt/src/main.rs
+++ rust_yt/src/main.rs
@@ -1,7 +1,11 @@
 mod module_01_data_types_examples;
 mod module_02_control_flows_examples;
+mod module_03_functions_closure_examples;
 
 fn main() {
     // module_01_data_types_examples::demo();
-    module_02_control_flows_examples::demo();
+    // module_02_control_flows_examples::demo();
+    module_03_functions_closure_examples::demo();
+
+    println!("\n\n\n");
 }
--- rust_yt/target/debug/deps/rust_yt-558ab605b5573da2.d
+++ rust_yt/target/debug/deps/rust_yt-558ab605b5573da2.d
@@ -1,7 +1,8 @@
-/home/dayda/Rust_Enhancement/rust_yt/target/debug/deps/rust_yt-558ab605b5573da2.d: src/main.rs src/module_01_data_types_examples.rs src/module_02_control_flows_examples.rs
+/home/dayda/Rust_Enhancement/rust_yt/target/debug/deps/rust_yt-558ab605b5573da2.d: src/main.rs src/module_01_data_types_examples.rs src/module_02_control_flows_examples.rs src/module_03_functions_closure_examples.rs
 
-/home/dayda/Rust_Enhancement/rust_yt/target/debug/deps/rust_yt-558ab605b5573da2: src/main.rs src/module_01_data_types_examples.rs src/module_02_control_flows_examples.rs
+/home/dayda/Rust_Enhancement/rust_yt/target/debug/deps/rust_yt-558ab605b5573da2: src/main.rs src/module_01_data_types_examples.rs src/module_02_control_flows_examples.rs src/module_03_functions_closure_examples.rs
 
 src/main.rs:
 src/module_01_data_types_examples.rs:
 src/module_02_control_flows_examples.rs:
+src/module_03_functions_closure_examples.rs:
