f3b476084af8af5f71978d10309b3328460a880f
wls503pl <peile.wu.1990@gmail.com>
2026-03-26 09:58:47 +0800

    Chapter 10: Option & Result.

--- rust_yt/src/module_10_option_result_combination.rs
+++ rust_yt/src/module_10_option_result_combination.rs
@@ -1,2 +1,24 @@
 pub fn demo()
-{}
+{
+    let successful_transactions:Option<Result<&str, &str>>  = Some(Ok("Transaction successful!"));
+    let failed_transaction: Option<Result<&str, &str>> = Some(Err("Transaction failed due to insufficient funds!"));
+    let no_transaction: Option<Result<&str, &str>> = None;
+
+    match successful_transactions {
+        Some(Ok(message)) => println!("Success: {}", message),
+        Some(Err(message)) => println!("Error: {}", message),
+        None => println!("No transaction result."),
+    }
+
+    match failed_transaction {
+        Some(Ok(message)) => println!("Success: {}", message),
+        Some(Err(message)) => println!("Error: {}", message),
+        None => println!("No transaction result."),
+    }
+
+    match no_transaction {
+        Some(Ok(message)) => println!("Success: {}", message),
+        Some(Err(message)) => println!("Error: {}", message),
+        None => println!("No transaction result."),
+    }
+}
